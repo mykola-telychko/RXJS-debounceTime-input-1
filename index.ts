1eb08071f00eb5aea3c239a3bd222d5ab0b70303
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-20 23:11:25 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
 import { fromEvent } from 'rxjs';
 import { debounceTime, map } from 'rxjs/operators';
 
-// elem ref
+// https://www.learnrxjs.io/learn-rxjs/operators/filtering/debouncetime
+
+// Example 1: Debouncing based on time between input
 const searchBox = document.getElementById('search');
 
 // streams
-const keyup$ = fromEvent(searchBox, 'keyup')
+const keyup$ = fromEvent(searchBox, 'keyup');
 
 // wait .5s between keyups to emit current value
-keyup$.pipe(
-  map((i: any) => i.currentTarget.value),
-  debounceTime(500)
-)
-.subscribe(console.log);
+keyup$
+  .pipe(
+    map((i: any) => i.currentTarget.value),
+    debounceTime(500)
+  )
+  .subscribe(console.log);
